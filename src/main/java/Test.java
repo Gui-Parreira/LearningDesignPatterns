e386e1f58a0c168bc6134c4e69927f103d04d188
Gui-Parreira <gui.parreira98@gmail.com>
2024-06-24 20:41:43 -0300

    Implementação Strategy Design Pattern

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,12 +1,13 @@
-import one.digitalinnovation.gof.SingletonEager;
-import one.digitalinnovation.gof.SingletonLazy;
-import one.digitalinnovation.gof.SingletonLazyHolder;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        //Testes relacionados ao Design Pattern Singleton
+        //Singleton
 
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
@@ -22,5 +23,22 @@ public class Test {
         System.out.println(lazyHolder);
         lazyHolder = SingletonLazyHolder .getInstancia();
         System.out.println(lazyHolder);
+
+        //Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
     }
 }
